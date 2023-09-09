3e25ef0d7fba8b5cc72829885b2f2b7e203438d3
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2023-09-09 23:03:05 +0900

    add files

--- tango_ext/integrations/diffusers/__init__.py
+++ tango_ext/integrations/diffusers/__init__.py
@@ -1,1 +1,14 @@
-from tango_ext.integrations.diffusers.format import DiffusersPipelineFormat  # NOQA
+from tango.common.exceptions import IntegrationMissingError
+
+try:
+    import diffusers  # NOQA
+except ModuleNotFoundError:
+    raise IntegrationMissingError("diffusers")
+
+
+from .format import DiffusersPipelineFormat  # NOQA
+from .pipeline import (  # NOQA
+    DiffusersPipelineForImage2Image,
+    DiffusersPipelineForInpainting,
+    DiffusersPipelineForText2Image,
+)
