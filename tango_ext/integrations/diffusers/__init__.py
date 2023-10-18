17d76d849826c5ceb10e11d10b67fd4c994c73cd
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2023-10-19 00:02:30 +0900

    raise IntegrationMissingError (#9)

--- tango_ext/integrations/diffusers/__init__.py
+++ tango_ext/integrations/diffusers/__init__.py
@@ -1,1 +1,8 @@
+from tango.common.exceptions import IntegrationMissingError
+
+try:
+    import diffusers  # NOQA
+except ModuleNotFoundError:
+    raise IntegrationMissingError("diffusers")
+
 from tango_ext.integrations.diffusers.format import DiffusersPipelineFormat  # NOQA
