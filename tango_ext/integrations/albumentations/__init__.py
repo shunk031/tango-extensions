5cc1db8079029179e4b5d146d5757575bc0f35fe
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2023-10-18 22:13:27 +0900

    update

--- tango_ext/integrations/albumentations/__init__.py
+++ tango_ext/integrations/albumentations/__init__.py
@@ -1,2 +1,9 @@
+from tango.common.exceptions import IntegrationMissingError
+
+try:
+    import albumentations  # NOQA
+except ModuleNotFoundError:
+    raise IntegrationMissingError("albumentations")
+
 from .core import *  # NOQA
 from .augmentations import *  # NOQA
